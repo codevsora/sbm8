ea98ed93d45ca6ebd2919b277d0f211d3e1d8e8b
codevsora <xylol.eli@gmail.com>
2026-09-18 12:45:05 +0300

    fix: correct formatting flagged by Spotless

--- src/main/java/ee/tak24/kursusepunkt/course/CourseController.java
+++ src/main/java/ee/tak24/kursusepunkt/course/CourseController.java
@@ -24,9 +24,9 @@ public class CourseController {
 
   private final CourseService courseService;
 
-CourseController(CourseService courseService) {
-this.courseService = courseService;
-}
+  CourseController(CourseService courseService) {
+    this.courseService = courseService;
+  }
 
   @Operation(summary = "List courses")
   @GetMapping
@@ -34,8 +34,8 @@ this.courseService = courseService;
     return courseService.findAll(pageable);
   }
 
-      @Operation(summary = "Fetch one course")
-          @GetMapping("/{id}")
+  @Operation(summary = "Fetch one course")
+  @GetMapping("/{id}")
   public CourseResponse get(@PathVariable Long id) {
     return courseService.findById(id);
   }
